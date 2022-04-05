31bd3ab343aa35549ca1a51b9c171f7fe3af8d98
Alex Vear <alex@vear.uk>
2022-04-05 11:02:23 +0100

    Optimise macroexpansion of `with-stubs`

--- src/uk/axvr/dynamock.clj
+++ src/uk/axvr/dynamock.clj
@@ -18,25 +18,21 @@
   "Register the below scope with a mocked fn."
   [fn mock & body]
   `(with-stub-scope
-     (if (:dynamic (meta #'~fn))
+     (if (:dynamic (meta (var ~fn)))
        (binding [~fn (~mock ~fn)]
          ~@body)
        (with-redefs [~fn (~mock ~fn)]
          ~@body))))
 
+(defmacro with-stubs
+  "Like with-stub but registers multiple stubs at once."
+  [stubs & body]
+  `(with-stub-scope
+     ~@(map (fn [s] `(stub! ~s)) stubs)
+     ~@body))
+
 (defmacro with-stub
   "Register a stub for tests in the body."
   [stub & body]
-  `(with-stub-scope
-     (stub! ~stub)
+  `(with-stubs [~stub]
      ~@body))
-
-(defmacro with-stubs
-  "Like with-stub but registers multiple stubs at once."
-  [[stub & stubs] & body]
-  (if (seq stubs)
-    `(with-stub ~stub
-       (with-stubs ~stubs
-         ~@body))
-    `(with-stub ~stub
-       ~@body)))
